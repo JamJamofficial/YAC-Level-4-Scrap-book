24fffaf8466f0ad77751c012becdeab1389648a0
JamJam <jamjamofficial@email.com>
2025-07-10 16:46:02 +0100

    Homework done

--- FastAPI Project/FastApiproj/main.py
+++ FastAPI Project/FastApiproj/main.py
@@ -1,15 +1,8 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
-
-
-    return {"message": "Hello World"} 
-
-@app.post("/")
-async def create_root():
-
-
-    return {"message": "Hello World"} 
+    return {"message": "Hello, World!"}  
